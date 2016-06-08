0e77d48a2d82a739af078aa6f2c3802d75e49e6e
mrFourhands <jeffreyforhan1998@gmail.com>
2016-06-07 20:47:03 -0400

    Graphics implementation
    Magnitude from the vector object is passed to the 3D model. Angle still
    needs to be represented, and the model must be tweaked slightly, so that
    it acts correctly when deformed.

--- Vectors Program/Assets/Scripts/VectorClass.cs
+++ Vectors Program/Assets/Scripts/VectorClass.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
-public class VectorClass : MonoBehaviour {
+//Split into an inheritence tree for generic vectors that branch into 3D, 2D, and 4D
+public class VectorClass {
 
     public GameObject vectorObj;
 
@@ -16,21 +17,31 @@ public class VectorClass : MonoBehaviour {
     //Holds the vector's magnitude
     private float magni;
 
-    //Constructors
-    //For 2d
-    public VectorClass(int x, int y) {
-        type = true;
+    ////Constructors
+    ////For 2d
+    //public VectorClass(int x, int y) {
+    //    type = true;
 
-        //Add vector components to an array
-        components = new float[2];
-        components[0] = x;
-        components[1] = y;
+    //    //Add vector components to an array
+    //    components = new float[2];
+    //    components[0] = x;
+    //    components[1] = y;
 
-        magni = magnify(components);
+    //    magni = magnify(components);
 
-    }
+    //}
+
     //For 3d
+    /// <summary>
+    /// Constructor*
+    /// Pre: User has pressed the add vector button in the GUI
+    /// Post: Makes a 3 - dimensional position vector, based on x, y, and z components
+    /// </summary>
+    /// <param name="x">X component</param>
+    /// <param name="y">Y component</param>
+    /// <param name="z">Z component</param>
     public VectorClass(int x, int y, int z) {
+        //This is an algebreic vector
         type = true;
 
         //Add vector components to an array
@@ -39,43 +50,82 @@ public class VectorClass : MonoBehaviour {
         components[1] = y;
         components[2] = z;
 
-        magni = magnify(components);
+        //Find the magnitude of the vector. Store this value.
+        magni = magnify();
 
     }
-    //For 4d
-    public VectorClass(float x, float y, float z, float w) {
+
+    //For 3d #2
+    /// <summary>
+    /// Constructor*
+    /// Pre: User has pressed the add vector button in the GUI
+    /// Post: Makes a 3 - dimensional displacement vector, based on x, y, and z components, as well as a displacement from the origin
+    /// </summary>
+    /// <param name="x">X component</param>
+    /// <param name="y">Y component</param>
+    /// <param name="z">Z component</param>
+    /// <param name="disp">The displacement from the origin</param>
+    public VectorClass(int x, int y, int z, Transform disp) {
+        //This is an algebreic vector
         type = true;
 
         //Add vector components to an array
-        components = new float[4];
+        components = new float[3];
         components[0] = x;
         components[1] = y;
         components[2] = z;
-        components[3] = w;
 
-        magni = magnify(components);
+        //Find the magnitude of the vector. Store this value.
+        magni = magnify();
 
     }
-    //For geometric
-    public VectorClass(float mag, float deg) {
-        type = false;
-        components = new float[2];
 
-        //Store the magnitude as the first array element and the direction in the second
-        components[0] = mag;
-        components[1] = deg;
+    ////For 4d
+    //public VectorClass(float x, float y, float z, float w) {
+    //    type = true;
 
-        magni = components[0];
+    //    //Add vector components to an array
+    //    components = new float[4];
+    //    components[0] = x;
+    //    components[1] = y;
+    //    components[2] = z;
+    //    components[3] = w;
 
+    //    magni = magnify(components);
+
+    //}
+    ////For geometric
+    //public VectorClass(float mag, float deg) {
+    //    type = false;
+    //    components = new float[2];
+
+    //    //Store the magnitude as the first array element and the direction in the second
+    //    components[0] = mag;
+    //    components[1] = deg;
+
+    //    magni = components[0];
+
+    //}
+
+    //Gets the vector components
+    public float[] getVectComp() {
+        return components;
+    }
+
+    //Resets the vector components
+    public void setVectComp(float x, float y, float z) {
+        components[0] = x;
+        components[1] = y;
+        components[2] = z;
     }
 
     //Gets the magnitude of the vector(DONE)
-    public float magnify(float[] componentsM) {
+    public float magnify() {
 
         float magnitude = 0f;
 
-        for(int i = 0; i < componentsM.Length; i++) {
-            magnitude += Mathf.Pow(componentsM[i], 2f);
+        for(int i = 0; i < components.Length; i++) {
+            magnitude += Mathf.Pow(components[i], 2f);
         }
         magnitude = Mathf.Sqrt(magnitude);
 
@@ -100,20 +150,20 @@ public class VectorClass : MonoBehaviour {
         }
         return dotVal;
     }
-    //geometric dot (DONE)
-    public float dot(float magA, float magB, float theta) {
-        float dotVal = 0;
+    ////geometric dot (DONE)
+    //public float dot(float magA, float magB, float theta) {
+    //    float dotVal = 0;
 
-        dotVal = magA * magB * Mathf.Cos(Mathf.Deg2Rad * theta);
+    //    dotVal = magA * magB * Mathf.Cos(Mathf.Deg2Rad * theta);
 
-        return dotVal;
-    }
-    //algebreic cross
+    //    return dotVal;
+    //}
+    //algebreic cross (DONE IN JAVA --- TRANSLATE)
     public int[] cross(VectorClass v1, VectorClass v2) {
         return null;
     }
-    //geometric cross
-    public int[] cross(float magA, float magB, float theta) {
-        return null;
-    }
+    ////geometric cross
+    //public int[] cross(float magA, float magB, float theta) {
+    //    return null;
+    //}
 }
---
+++ Vectors Program/Assets/Scripts/makeGraphic.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class makeGraphic : MonoBehaviour {
+
+    //WILL NEED TO BE CHANGED WHEN VECTORS CAN BE DELETED
+    //Number of times vectors have been created
+    int i = 0;
+
+    //The prefabricated vector object. Contains several 3D graphical components, and controlling invisible components.
+    public GameObject vectorModel;
+    private Vector3 position = new Vector3(0f,0f,0f);
+
+    //Holds all of the vector object structures
+    private VectorClass[] allVectors = new VectorClass[12];
+    //Holds all of the vector 3D models, components, gameobjects, and controlling empties
+    private GameObject[] allModels = new GameObject[12];
+    //Holds the object that controls the graphics of the vector tails
+    private GameObject[] allTails = new GameObject[12];
+
+
+    //Called by the GUI. When the make vector button is pressed, a vector is made. It's model is stored in one array, and its class is stored in another one.
+    public void make() {
+        /*When working with Euler angles, 
+            Rotations are COUNTER-CLOCKWISE
+            x = rotation around x-axis --- changes z component.
+            y = rotation around y-axis --- changes x AND y components.
+            z will not produce a visible change, in this case
+        */
+        if (i < allModels.Length) {//If there are fewer than 12 vectors
+
+            allVectors[i] = new VectorClass(50, 50, 50);
+            //Instantiate the stored model into the scene, and store a copy of it in an array
+            allModels[i] = (GameObject)Instantiate(vectorModel, position, Quaternion.Euler(270f, 0f, 0f));
+
+            //Modify the vector scale, along its forward axis, based on the vector magnitude
+            allModels[i].transform.localScale = new Vector3(1f,1f, allVectors[i].magnify());
+
+            //Rename a vector once it is created
+            allModels[i].name = "Vector [" + i + "]";
+
+            i++;
+
+            position.x++;
+        }else {//If there are more than 12 vectors...
+            Debug.Log("Don't go too crazy with the vectors!");
+        }
+    }
+}
